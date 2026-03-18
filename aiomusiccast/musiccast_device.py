ef8bd207e4dcbf1a8081a55eb054e9e199dd9304
crash0verride11 <3526616+crash0verride11@users.noreply.github.com>
2026-03-18 13:32:20 -0400

    fix: input_list iterating over None causing error
    * self._name_text.get("input_list") returns None, and iterating over None raises a TypeError
    * same is true for sound_program_list

--- aiomusiccast/musiccast_device.py
+++ aiomusiccast/musiccast_device.py
@@ -480,10 +480,10 @@ class MusicCastDevice:
                 self.data.alarm_preset_list = clock_features.get("alarm_preset_list", [])
                 self.data.alarm_resume_input_list = clock_features.get("alarm_input_list", [])
 
-        self.data.input_names = {source.get("id"): source.get("text") for source in self._name_text.get("input_list")}
+        self.data.input_names = {source.get("id"): source.get("text") for source in self._name_text.get("input_list", [])}
 
         self.data.sound_program_names = {
-            program.get("id"): program.get("text") for program in self._name_text.get("sound_program_list")
+            program.get("id"): program.get("text") for program in self._name_text.get("sound_program_list", [])
         }
 
         netusb_features = self._features.get("netusb", {})
