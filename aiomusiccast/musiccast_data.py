2e6b17f81002df74c5989301a563c8c13b6817dc
crash0verride11 <3526616+crash0verride11@users.noreply.github.com>
2026-03-28 12:42:42 -0400

    upstream parity
    add upstream fix

--- aiomusiccast/musiccast_data.py
+++ aiomusiccast/musiccast_data.py
@@ -209,6 +209,8 @@ class MusicCastZoneData:
 
         self.surr_decoder_type: str | None = None
 
+        self.scene_information: dict[int, str] | None = None
+
 
 @dataclass(slots=True)
 class RangeStep:
